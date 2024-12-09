edea829712fe5f334117a8882e93fac43065c52f
Jerwin Peralta <jerwin.tuchi3@gmail.com>
2024-12-09 12:28:52 +0800

    Initial Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+
+import { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Godstrength",
+  description: "Built by pure Strength.",
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png"],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}

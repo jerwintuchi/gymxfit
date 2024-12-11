d4373d7901fea83989a0f740100054557ddcf883
Jerwin Peralta <jerwin.tuchi3@gmail.com>
2024-12-11 19:03:38 +0800

    ftr: added prisma schema and removed the .env.local

--- src/app/(dashboard)/_components/navbar.tsx
+++ src/app/(dashboard)/_components/navbar.tsx
@@ -1,9 +1,11 @@
+import { NavbarRoutes } from "@/components/navbar-routes";
 import { MobileSidebar } from "./mobile-sidebar";
 
 export const Navbar = () => {
     return (
-        <div className="p-2 border-b h-full flex items-center bg-white z-50">
+        <div className="p-4 border-b h-full flex items-center bg-white z-50">
             <MobileSidebar />
+            <NavbarRoutes />
         </div>
     );
 }
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,7 +6,7 @@ import Sidebar from "./_components/sidebar";
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="h-full">
-            <div className="h-[70px] md:pl-30 fixed inset-y-0 w-full">
+            <div className="h-[70px] md:pl-30 fixed inset-y-0 w-full z-50">
                 <Navbar />
             </div>
             <div className="hidden md:flex h-full w-30 flex-col fixed inset-y-0 z-50">
@@ -15,7 +15,6 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
             <main className="md:pl-40 pt-[80px] h-fit flex min-h-screen">
                 {children}
             </main>
-
         </div>
     );
 }

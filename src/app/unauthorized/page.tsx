03ba602f06e7495099b87ca43ce42b4bbd994ceb
Jerwin Peralta <jerwin.tuchi3@gmail.com>
2025-01-12 16:38:10 +0800

    ui: redesigned the unauthorized page

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Link from 'next/link'; // If you're using Next.js for routing
 import { SignOutButton } from '@clerk/nextjs';
-
+import Image from 'next/image';
 const Unauthorized = () => {
     return (
-        <div className="min-h-screen bg-gray-900 flex flex-col justify-center items-center text-white">
-            <div className="max-w-md w-full p-6 bg-gray-800 rounded-lg shadow-md text-center">
+        <div className="min-h-screen bg-black flex flex-col justify-center items-center text-white">
+            <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-md text-center">
+                <div className='justify-items-center p-4'>
+                    <Image src={"/static/unauthorized-image.png"} alt="unauthorized" width={100} height={100} />
+                </div>
                 <h1 className="text-4xl font-bold mb-4 text-red-500">Unauthorized</h1>
-                <p className="text-gray-300 mb-6">
+                <p className="text-gray-500 mb-6">
                     You do not have permission to access this page.
                 </p>
                 <div className="flex flex-col sm:flex-row gap-4 justify-center">
-                    <Link href="/sign-in" className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400">
+                    <Link href="/sign-in" className="bg-gray-900 hover:bg-gray-500 text-white py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400">
                         <SignOutButton redirectUrl="/sign-in">Back to Sign In</SignOutButton>
                     </Link>
                 </div>
